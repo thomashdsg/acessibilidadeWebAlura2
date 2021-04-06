8fb40dde91cedb2bce283813c9e71ea06cd53584
unknown <financeiro@financeiro01.baladapp.lan>
2021-04-06 17:58:56 -0300

    Primeiro Commit

---
+++ js/dialog.js
@@ -0,0 +1,23 @@
+// Variáveis
+var btnAbreDialog = document.querySelector('#abreDialog');
+var dialog = document.querySelector('.dialogNewsletter');
+var dialogBody = document.querySelector('.dialogNewsletter-body');
+var dialogOverlay = document.querySelector('.dialogNewsletter-overlay');
+
+
+btnAbreDialog.style.display = 'block';
+
+// Quando abrir a dialog...
+btnAbreDialog.addEventListener('click', function() {
+  dialog.classList.add('dialogNewsletter--aberto');
+  document.querySelector('.dialogNewsletter-campo').focus();
+  document.querySelector('#conteudoForaDialog').inert = true;
+});
+
+function fechandoDialog() {
+  document.activeElement.blur();
+  dialog.classList.remove('dialogNewsletter--aberto');     
+}
+
+// Listeners
+document.querySelector('.dialogNewsletter-fechar').addEventListener('click', fechandoDialog);
